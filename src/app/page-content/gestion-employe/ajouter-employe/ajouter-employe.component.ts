cfd6714683e4648c12267eb646734b35d76f7e50
ismail-labbi <47818608+ismail-labbi@users.noreply.github.com>
2019-04-11 23:31:03 +0200

    employees.json

--- src/app/page-content/gestion-employe/ajouter-employe/ajouter-employe.component.ts
+++ src/app/page-content/gestion-employe/ajouter-employe/ajouter-employe.component.ts
@@ -5,6 +5,7 @@ import { Employe} from './../../Models/Employe';
 import {EmployeesService} from './../../Services/employees.service';
 import {EmployeesJsonService} from '../../Services/employees-json.service';
 import {Output,EventEmitter} from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
 @Component({
   selector: 'app-ajouter-employe',
   templateUrl: './ajouter-employe.component.html',
@@ -19,7 +20,7 @@ export class AjouterEmployeComponent implements OnInit {
    private Salary:string;
    private email:string;
  
-  constructor(public EmpServiceJson:EmployeesJsonService) { }
+  constructor(public EmpServiceJson:EmployeesJsonService,private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
   
@@ -27,7 +28,7 @@ export class AjouterEmployeComponent implements OnInit {
   }
 
 
-  Mysubmit(){
+  Mysubmit(a,b:HTMLInputElement){
     this.employe=new Employe();
     this.employe.generateId();
     this.employe.setPrenom(this.Prenom);
@@ -37,8 +38,25 @@ export class AjouterEmployeComponent implements OnInit {
     parseFloat
     this.employe.setSalary(parseFloat(this.Salary));
 
-    this.EmpServiceJson.ajouterEmployee(this.employe).subscribe();    
+    this.EmpServiceJson.ajouterEmployee(this.employe).subscribe(
+      x=>{    this.flashMessage.show(' opération réussie', { cssClass: 'alert-success', timeout: 2000 });
+      
+   
+   this.viderlestextes();
+   
+      
+    }
+    );    
   }
-  
+  viderlestextes(){
+    this.Age="";
+    this.email="";
+    this.Nom="";
+    this.Prenom="";
+    this.Salary="";
+  }
+
+
+
 
 }
